4a5d923803a7909f150d3f1828c7c4a2e1068368
lasselupe33 <lasse.lupe@gmail.com>
2019-02-25 13:27:10 +0100

    Added support for import SVG's and added folders for basic architecture

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { getSVG } from 'src/assets/svg';
 
 export class App extends React.PureComponent {
 	public render(): JSX.Element {
+
 		return (
 			<div>
 				Hello World
+				{ getSVG("logo", {fillHoverColor: "red", transitionDuration: 1000}) }
 
 				<style jsx>{`
 					div {
-						font-size: 1000px;
+						font-size: 50px;
 					}
 				`}</style>
 			</div>
---
+++ src/ts/config/fonts.ts
@@ -0,0 +1,6 @@
+/**
+ * Collection of font families used throughout the application.
+ */
+export const fonts = {
+    franklin: `helvetica, arial, sans-serif`,
+};
