7646ce0735d20db785684d02ba58b41c803d36c4
steinkat <christina.spost@gmail.com>
2019-02-25 15:01:34 +0100

    initial markup and css

--- src/ts/components/elements/Application/Application.tsx
+++ src/ts/components/elements/Application/Application.tsx
@@ -5,12 +5,72 @@ export class Application extends React.PureComponent {
 
 		return (
 			<div>
-				Hello Application
+				
+                <div className="application-border">
+                    <div className="application">
+                        <div className="section-thumbnail">
+                            <div className="thumbnail" />
+                            <div className="name">Christina S</div>
+                        </div>
+                        
+                        <div className="section">
+                            <div className="product">5 chickens</div>
+                            <div className="country">China</div>
+                        </div>
+
+                        <div className="section">
+                            <div className="price">$10</div>
+                            <button className="donateButton">Donate</button>
+                        </div>
+                        
+                    </div>
+                </div>
+                
 
 				<style jsx>{`
-					div {
-						font-size: 50px;
+                    .application-border {
+                        height: 120px;
+                        width: 400px;
+                        padding: 10px 10px 0px 10px;
+                        border: 1px solid lightgrey;
+                    }
+
+                    .application-border:hover {
+                        background-color: lightgrey
+                    }
+
+					.application {
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: space-between;
+                        font-size: 30px;
 					}
+
+                    .section-thumbnail {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                        font-size: 18px;
+                    }
+
+                    .section {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                    }
+
+                    .thumbnail {
+                        margin-bottom: 10px;
+                        height: 75px;
+                        width: 75px;
+                        background-color: #555;
+                        border-radius: 50%;
+                    }
+
+                    .price {
+                        text-align: right;
+                    }
+
 				`}</style>
 			</div>
 		);
