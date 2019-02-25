8b54db0d0660768934bd18f5606e6059339c64f4
steinkat <christina.spost@gmail.com>
2019-02-25 14:00:22 +0100

    commit to do stuff

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-import { getSVG } from 'src/assets/svg';
-
-
 export class App extends React.PureComponent {
 	public render(): JSX.Element {
-
 		return (
 			<div>
 				Hello World
-				{ getSVG("logo", {fillHoverColor: "red", transitionDuration: 1000}) }
 
 				<style jsx>{`
 					div {
