8f1a2bbe1038d3016715eb2cf876a85b69d2f671
Trine Borre Jørgensen <trineborre@Trines-MacBook-Air.local>
2019-03-20 10:54:03 +0100

    started work

--- src/ts/config/apis.ts
+++ src/ts/config/apis.ts
@@ -1,10 +1,10 @@
-const basePath = "https://api.pollopollo.org";
+const basePath = "https://api.pollopollo.org/api";
 
 export const apis = {
     user: {
         create: `${basePath}/users/`,
         get: `${basePath}/users/{userId}`,
-        authenticate: `${basePath}/users/`
+        authenticate: `${basePath}/users/authenticate`
     },
     application: {
         getRecent: `${basePath}/applications/recent`
