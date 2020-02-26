38a298fa9dd2ad91eea6927f6e1878e12cef228e
psiphph <sifamalie@gmail.com>
2020-02-26 08:45:58 +0100

    Fixed keep sorting when filtering on applications page

--- src/ts/components/pages/ApplicationsPage/ApplicationsPage.tsx
+++ src/ts/components/pages/ApplicationsPage/ApplicationsPage.tsx
@@ -541,6 +541,7 @@ class UnwrappedApplicationsPage extends React.PureComponent<ApplicationsPageProp
             applications: response.applications,
             totalApplications: response.count,
         });
+        this.sortApplications(this.state.sortBy);
         this.setState({isPending: false});
     }
 
